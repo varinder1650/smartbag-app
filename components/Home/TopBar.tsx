1c98400a5f901716b3eb5918720c526a4fc2b670
Nitin Goyal <nitingoyal3150@gmail.com>
2026-04-19 23:13:13 -0400

    Added the brand name, delivery time, and address label in the top section

--- components/Home/TopBar.tsx
+++ components/Home/TopBar.tsx
@@ -5,9 +5,11 @@ import NotificationIcon from "./NotificationsIcon";
 
 export default function TopBar(){
     return(
-        <View className="flex-row items-center pb-2">
-            <Address/>
-            <View className="flex-row items-center gap-4">
+        <View className="flex-row items-start justify-between pb-2 px-4 pt-2">
+            <View className="flex-1 mr-4">
+                <Address/>
+            </View>
+            <View className="flex-row items-center gap-4 mt-2">
                 <NotificationIcon/>
                 <CartIcon/>
             </View>
